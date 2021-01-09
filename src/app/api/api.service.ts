0c0dc6349467fb94a983f06e4929aa4034b20976
sajithahd <hdsajitha@gmail.com>
2021-01-09 22:23:44 +0530

    1 file modified

--- src/app/api/api.service.ts
+++ src/app/api/api.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { Post } from "../models/post";
@@ -7,9 +7,24 @@ import { Post } from "../models/post";
 export class APIService {
   constructor(private http: HttpClient) {}
 
+  private httpOptions = {
+    headers: new HttpHeaders({
+      "Content-Type": "application/json",
+      charset: "UTF-8"
+    })
+  };
+
   getPosts(): Observable<Post[]> {
     return this.http.get<Post[]>(
       "https://jsonplaceholder.typicode.com/posts"
     ) as Observable<Post[]>;
+  }
+
+  addPost(post: Post): Observable<Post> {
+    return this.http.post(
+      "https://jsonplaceholder.typicode.com/posts",
+      post,
+      this.httpOptions
+    ) as Observable<Post>;
   }
 }
