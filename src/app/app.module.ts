98e3a199d9f28e9aa24439735e61dace85ecc244
sajithahd <hdsajitha@gmail.com>
2021-01-09 19:35:24 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { PostsComponent } from './posts.component';
 import { FacadeService } from './facade.service';
 import { StateService } from './state/state.service';
 import { APIService } from './api/api.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, HttpClientModule ],
   declarations: [ AppComponent, PostsComponent ],
   bootstrap:    [ AppComponent ],
   providers: [FacadeService, StateService, APIService]
