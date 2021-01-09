399ee050dc681b7b355781973efd427fe9cdbed7
sajithahd <hdsajitha@gmail.com>
2021-01-09 19:20:24 +0530

    1 file modified

--- src/app/api/api.service.ts
+++ src/app/api/api.service.ts
@@ -1,7 +1,14 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { Post } from "../models/post";
 
 @Injectable()
 export class APIService {
   constructor(private http: HttpClient) {}
+
+  getPosts(): Observable<Post[]> {
+    this.http.get<Post[]>("") as Observable<Post[]>;
+    return null;
+  }
 }
