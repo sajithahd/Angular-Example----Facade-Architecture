5feeecb793cfcc87bec017cf464605fba5fdad9e
sajithahd <hdsajitha@gmail.com>
2021-01-09 19:28:50 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { FacadeService } from './facade.service';
+import { StateService } from './state/state.service';
+import { APIService } from './api/api.service';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ],
-  providers: [FacadeService]
+  providers: [FacadeService, StateService, APIService]
 })
 export class AppModule { }
