4955f689623869648dba78d99d2ef5d6988a5cd7
sajithahd <hdsajitha@gmail.com>
2021-01-09 23:11:39 +0530

    1 file modified

--- src/app/add-new-post.component.ts
+++ src/app/add-new-post.component.ts
@@ -1,9 +1,23 @@
 import { Component } from "@angular/core";
+import { FacadeService } from "./facade.service";
+import { Post } from "./models/post";
 
 @Component({
-  selector: 'add-new-post',
-  template:`
+  selector: "add-new-post",
+  template: `
     <div>Add new Post</div>
   `
 })
-export class AddNewPostComponent {}
+export class AddNewPostComponent {
+  constructor(private facadeService: FacadeService) {
+    let post: Post = {
+      userId: 1,
+      id: 1,
+      title:
+        "My test post",
+      body:
+        "My first post regaring the facade architecture for angular."
+    } as Post;
+    facadeService.addPost(post);
+  }
+}
